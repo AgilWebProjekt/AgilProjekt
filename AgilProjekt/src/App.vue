e8708df0899c10bb79a480850052133dbd7e7466
Osedumme Steven <stephen.osedumme@iths.se>
2023-10-11 12:06:53 +0200

    changed appvue

--- AgilProjekt/src/App.vue
+++ AgilProjekt/src/App.vue
@@ -1,9 +1,16 @@
 <script setup>
+import HomePage from './components/HomePage.vue';
 </script>
 
 <template>
-  <div></div>
+  <HomePage />
 </template>
 
 <style scoped>
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+}
+body {
+   background-color: #72a61d;
+ }
 </style>
