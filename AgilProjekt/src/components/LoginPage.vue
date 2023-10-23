c307f2fc3da1f35afe0804c11ae65695e2c4107e
Osedumme Steven <stephen.osedumme@iths.se>
2023-10-23 10:01:32 +0200

    created login page

--- AgilProjekt/src/components/LoginPage.vue
+++ AgilProjekt/src/components/LoginPage.vue
@@ -11,6 +11,34 @@
   </div>
 </template>
 
-<style></style>
+<style>
+h1 {
+  color: white;
+  text-align: center;
+}
+.register input {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-right: auto;
+  margin-left: auto;
+  border: 1px solid skyblue;
+}
+.register button {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-right: auto;
+  margin-left: auto;
+  border: 1px solid skyblue;
+  background: skyblue;
+  color: #fff;
+  cursor: pointer;
+}
+</style>
 
 
