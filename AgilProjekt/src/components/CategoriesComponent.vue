42c076ce5ed6673d7f9cae0f40fbe6594d69d717
VenuModi <venu.modi@iths.se>
2023-10-27 14:00:04 +0200

    Fix syntax error in database

---
+++ AgilProjekt/src/components/CategoriesComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Math Questions</h1>
+        <ul>
+            <li v-for="question in mathQuestions" :key="question.mathId">
+                {{ question.mathQuestion }}
+            </li>
+        </ul>
+    </div>
+</template>
+     
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const mathQuestions = ref([]);
+
+onMounted(() => {
+    fetchMathQuestions();
+});
+
+const fetchMathQuestions = async () => {
+    try {
+        const response = await axios.get('http://localhost:3000/api/mathematics'); // Replace with your server URL
+        mathQuestions.value = response.data;
+    } catch (error) {
+        console.error('Error fetching math questions:', error);
+    }
+};
+</script>
--- AgilProjekt/src/views/CategoriesView.vue
+++ AgilProjekt/src/views/CategoriesView.vue
@@ -1,23 +1,21 @@
 <script setup>
-
+import Categories from '../components/CategoriesComponent.vue'
 
 </script>
 
 
 <template>
-    <div class="category">
-      <h1>This is categories page</h1>
-    </div>
+  <div class="category">
+    <h1>This is categories page</h1>
+  </div>
 </template>
   
 <style>
-  @media (min-width: 1024px) {
-    .category {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
-    }
+@media (min-width: 1024px) {
+  .category {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
 }
-
-
 </style>
