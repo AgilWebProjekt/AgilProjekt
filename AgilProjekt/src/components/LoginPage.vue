53df92781fbf29a88c919de1656690100754a829
Osedumme Steven <stephen.osedumme@iths.se>
2023-10-29 13:46:59 +0100

    editing branch

--- AgilProjekt/src/components/LoginPage.vue
+++ AgilProjekt/src/components/LoginPage.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 
 import { ref } from 'vue';
 import { useRouter /*useRoute*/ } from 'vue-router'
@@ -77,9 +77,65 @@ h1 {
   cursor: pointer;
 }
 </style>
+ -->
 
+<template>
+  <div>
+    <div v-if="!loggedIn">
+      <h2>Register</h2>
+      <input v-model="registerData.name" placeholder="name" />
+      <input v-model="registerData.email" placeholder="email" />
+      <input v-model="registerData.username" placeholder="Username" />
+      <input v-model="registerData.password" type="password" placeholder="Password" />
+      <button @click="register">Register</button>
 
-  
+      <h2>Login</h2>
+      <input v-model="loginData.username" placeholder="Username" />
+      <input v-model="loginData.password" type="password" placeholder="Password" />
+      <button @click="login">Login</button>
+    </div>
+    <div v-if="loggedIn">
+      <h2>Submit Feedback</h2>
+      <textarea v-model="feedback"></textarea>
+      <button @click="submitFeedback">Submit</button>
+    </div>
+  </div>
+</template>
 
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
 
+const registerData = ref({ name: '', email: '', username: '', password: '' })
+const loginData = ref({ username: '', password: '' })
+const loggedIn = ref(false)
+const feedback = ref('')
 
+const register = async () => {
+  try {
+    await axios.post('http://localhost:3000/register', registerData.value)
+    alert('Registered successfully')
+  } catch (err) {
+    alert(err.response?.data?.error || 'Unknown error')
+  }
+}
+
+const login = async () => {
+  try {
+    await axios.post('http://localhost:3000/login', loginData.value)
+    loggedIn.value = true
+  } catch (err) {
+    alert(err.response?.data?.error || 'Unknown error')
+  }
+}
+
+const submitFeedback = async () => {
+  try {
+    await axios.post('http://localhost:3000/feedback', { feedback: feedback.value })
+    alert('Feedback submitted')
+  } catch (err) {
+    alert('Failed to submit feedback')
+  }
+}
+</script>
+-->
