1bfece55f78b2b2487f95a4432cdcfd6b6122376
Osedumme Steven <stephen.osedumme@iths.se>
2023-11-02 02:49:12 +0100

    amends

--- AgilProjekt/src/components/RegisterPage.vue
+++ AgilProjekt/src/components/RegisterPage.vue
@@ -2,25 +2,56 @@
 import { Form, Field } from 'vee-validate'
 import * as Yup from 'yup'
 
-//import { useAuthStore } from '@/stores'
+//import { useUsersStore, useAlertStore } from '@/stores'
+//import { router } from '@/router'
 
 const schema = Yup.object().shape({
+  firstName: Yup.string().required('First Name is required'),
+  lastName: Yup.string().required('Last Name is required'),
   username: Yup.string().required('Username is required'),
-  password: Yup.string().required('Password is required')
+  password: Yup.string()
+    .required('Password is required')
+    .min(6, 'Password must be at least 6 characters')
 })
 
 /*async function onSubmit(values) {
-  const authStore = useAuthStore()
-  const { username, password } = values
-  await authStore.login(username, password)
+  const usersStore = useUsersStore()
+  const alertStore = useAlertStore()
+  try {
+    await usersStore.register(values)
+    await router.push('/account/login')
+    alertStore.success('Registration successful')
+  } catch (error) {
+    alertStore.error(error)
+  }
 }*/
 </script>
 
 <template>
   <div class="card m-3">
-    <h2 class="card-header">Login</h2>
+    <h2 class="card-header">Register</h2>
     <div class="card-body">
       <Form @submit="onSubmit" :validation-schema="schema" v-slot="{ errors, isSubmitting }">
+        <div class="form-group">
+          <label>First Name</label>
+          <Field
+            name="firstName"
+            type="text"
+            class="form-control"
+            :class="{ 'is-invalid': errors.firstName }"
+          />
+          <div class="invalid-feedback">{{ errors.firstName }}</div>
+        </div>
+        <div class="form-group">
+          <label>Last Name</label>
+          <Field
+            name="lastName"
+            type="text"
+            class="form-control"
+            :class="{ 'is-invalid': errors.lastName }"
+          />
+          <div class="invalid-feedback">{{ errors.lastName }}</div>
+        </div>
         <div class="form-group">
           <label>Username</label>
           <Field
@@ -44,9 +75,9 @@ const schema = Yup.object().shape({
         <div class="form-group">
           <button class="btn btn-primary" :disabled="isSubmitting">
             <span v-show="isSubmitting" class="spinner-border spinner-border-sm mr-1"></span>
-            Login
+            Register
           </button>
-          <router-link to="register" class="btn btn-link">Register</router-link>
+          <router-link to="login" class="btn btn-link">Cancel</router-link>
         </div>
       </Form>
     </div>
--- AgilProjekt/src/router/index.js
+++ AgilProjekt/src/router/index.js
@@ -36,6 +36,14 @@ const router = createRouter({
       component: () => import('../views/LoginView.vue')
     },
     {
+      path: '/register',
+      name: 'register',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/RegisterView.vue')
+    },
+    {
       path: '/categories',
       name: 'categories',
       // route level code-splitting
