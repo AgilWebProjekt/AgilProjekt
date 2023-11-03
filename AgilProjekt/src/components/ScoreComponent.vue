fa2b9611e0f12647876715edc3eed55942f7745c
RehasLiamsi <nsaher91@gmail.com>
2023-11-03 19:54:01 +0100

    Finalize screenshot and scoreboard

--- AgilProjekt/src/components/ScoreComponent.vue
+++ AgilProjekt/src/components/ScoreComponent.vue
@@ -1,27 +1,8 @@
 <script setup>
-import { ref } from 'vue';
-import html2canvas from 'html2canvas';
+import { ref } from 'vue'
+import html2canvas from 'html2canvas'
 
-const scoreboard = ref(null);
-
-const takeScreenshot = async () => {
-  if (scoreboard.value) {
-    const canvas = await html2canvas(scoreboard.value);
-    const image = canvas.toDataURL('image/png');
-    downloadImage(image, 'scoreboard.png');
-  }
-};
-
-const downloadImage = (blob, filename) => {
-  const link = document.createElement('a');
-  link.href = blob;
-  link.download = filename;
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-};
-
-const props = defineProps({
+defineProps({
   score: {
     type: Number,
     required: true
@@ -35,36 +16,62 @@ const props = defineProps({
     required: true
   }
 })
+
+const scoreboard = ref(null)
+
+const takeScreenshot = async () => {
+  if (scoreboard.value) {
+    const canvas = await html2canvas(scoreboard.value)
+    const image = canvas.toDataURL('image/png')
+    downloadImage(image, 'quiztasticScoreboard.png')
+  }
+}
+
+const downloadImage = (blob, filename) => {
+  const link = document.createElement('a')
+  link.href = blob
+  link.download = filename
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+}
 </script>
 
 <template>
-    <div ref="scoreboard" class="scoreboard" v-if="quizCompleted">
-    <div class="score" >
-      <h2>You rocked at Quiztastic!</h2>
-      <p>
-        You scored <span>{{ props.score }} / {{ totalQuestions }}</span> questions correct
-      </p>
-      <button @click="takeScreenshot">Save the scoreboard</button>
+  <div class="complete-area" v-if="quizCompleted">
+    <div ref="scoreboard" class="scoreboard">
+      <div class="score">
+        <h2>You rocked at Quiztastic!</h2>
+        <p>
+          You scored <span>{{ score }} / {{ totalQuestions }}</span> questions correct
+        </p>
+      </div>
     </div>
-    </div>
-  </template>
- 
-  <style scoped>
-  .score {
-    background-color: #008170;
-    padding: 5rem;
-    border-radius: 0.5rem;
-    color: white;
-  }
-  
-  .score p {
-    font-size: 30px;
-  }
-  
-  .score span {
-    background-color: #2cce7d;
-    padding: 2px 5px;
-    color: black;
-  }
-  </style>
-  
+    <button id="screenshot-button" @click="takeScreenshot">Save the scoreboard</button>
+  </div>
+</template>
+
+<style scoped>
+.complete-area {
+  background-color: #008170;
+  padding: 2rem;
+  border-radius: 0.5rem;
+  color: white;
+  display: flex;
+  flex-direction: column;
+}
+
+h2{
+    font-size: 2rem;
+}
+
+.score p {
+  font-size: 1.4rem;
+}
+
+.score span {
+  background-color: #2cce7d;
+  padding: 2px 5px;
+  color: black;
+}
+</style>
