fdaf6604c3f730346cf461d8db67f02e2dfae8ed
RehasLiamsi <nsaher91@gmail.com>
2023-11-03 18:10:17 +0100

    Shift score to a separate component

---
+++ AgilProjekt/src/components/ScoreComponent.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+const props = defineProps({
+  score: {
+    type: Number,
+    required: true
+  },
+  totalQuestions: {
+    type: Number,
+    required: true
+  },
+  quizCompleted: {
+    type: Boolean,
+    required: true
+  }
+})
+</script>
+
+<template>
+    <div class="score" v-if="quizCompleted">
+      <h2>You have finished the quiz!</h2>
+      <p>
+        You scored <span>{{ props.score }} / {{ totalQuestions }}</span> questions correct
+      </p>
+    </div>
+  </template>
+ 
+  <style scoped>
+  .score {
+    background-color: #008170;
+    padding: 5rem;
+    border-radius: 0.5rem;
+    color: white;
+  }
+  
+  .score p {
+    font-size: 30px;
+  }
+  
+  .score span {
+    background-color: #2cce7d;
+    padding: 2px 5px;
+    color: black;
+  }
+  </style>
+  
