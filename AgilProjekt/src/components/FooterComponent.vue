0ce0a2dcbb2345a5ca41c41fb6c331ba24bf30ef
Osedumme Steven <stephen.osedumme@iths.se>
2023-11-05 12:27:00 +0100

    created class welcome and style its min-height

--- AgilProjekt/src/components/FooterComponent.vue
+++ AgilProjekt/src/components/FooterComponent.vue
@@ -1,49 +1,57 @@
 <template>
-    <footer class="page-footer">
-        <p>
-            &copy;
-            <a class = "github" href="https://github.com/AgilWebProjekt" target="_blank">Quiztastic</a>
-            built by Team Remote |
-            <a class = "contact" href="/contact" target="_self">Contact Us</a>
-        </p>
-    </footer>
+  <footer class="page-footer">
+    <p>
+      &copy;
+      <a class="github" href="https://github.com/AgilWebProjekt" target="_blank">Quiztastic</a>
+      built by Team Remote |
+      <a class="contact" href="/contact" target="_self">Contact Us</a>
+    </p>
+  </footer>
 </template>
-  
-<script setup>
-</script>
-  
+
+<script setup></script>
+
 <style scoped>
+/*.page-footer {
+  height: 4rem;
+  background-color: #005b41a7;
+  color: aliceblue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: auto;
+  min-height: 100vh;
+}*/
+
 .page-footer {
-    height: 4rem;
-    background-color: #005b41a7;
-    color: aliceblue;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-top: 2rem;
+  background-color: #005b41a7;
+  color: aliceblue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: auto;
 }
+
 .contact:hover {
-    color: black; 
+  color: black;
 }
 
 .github:hover {
-    color: black; 
+  color: black;
 }
-.contact{
-    color: white;
-    transition: color 0.3s;
-    margin: 0;
-
+.contact {
+  color: white;
+  transition: color 0.3s;
+  margin: 0;
 }
 
 .github {
-    color: white;
-    transition: color 0.3s;
-    margin: 0 1rem;
-    
+  color: white;
+  transition: color 0.3s;
+  margin: 0 1rem;
 }
 
 .page-footer a::visited {
-    color: aliceblue;
+  color: aliceblue;
 }
 </style>
